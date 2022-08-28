8cb1254c4aaf9fd6e2fd47330d6a92217bb913d4
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-28 17:42:29 +0530

    add Attandance

---
+++ EmployeeCalculation/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeCalculation
+{
+    internal class Employee
+    {
+        int isPrasent = 1;
+        Random random = new Random();
+
+        public void Attendence()
+        {
+
+            int result = random.Next(0, 2);
+            if (result == isPrasent)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeCalculation/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeCalculation;
+Employee empname = new Employee();
+empname.Attendence();
+
